aa484cdecc3d91fd3183b289ee87ee2a436da3c2
Miguel Coria <mcoria08@gmail.com>
2022-09-22 19:31:35 -0600

    Primer commit

---
+++ resources/views/slider.blade.php
@@ -0,0 +1,106 @@
+<section id="work" class="work_area bg-gray pt-120 pb-120">
+    <div class="container-fluid">
+        <div class="work_wrapper relative">
+            <div class="row work_active">
+                <div class="w-full lg:w-3/12">
+                    <div class="single_item mx-auto">
+                        <div class="single_work mx-3">
+                            <div class="work_image">
+                                <img src="assets/images/Aqua-Fit-Class.jpg" alt="work" class="w-full">
+                            </div>
+                            <div class="work_content">
+                                <a href="#" class="arrow"><i class="lni lni-chevron-right"></i></a>
+                                <h4 class="work_title text-xl md:text-2xl"><a href="#">Aqua Fit Class</a></h4>
+                                <p class="mt-2">Beach Club</p>
+                            </div>
+                        </div>
+                    </div> <!-- single work -->
+                </div>
+                <div class="w-full lg:w-3/12">
+                    <div class="single_item mx-auto">
+                        <div class="single_work mx-3">
+                            <div class="work_image">
+                                <img src="assets/images/yoga.jpg" alt="work" class="w-full">
+                            </div>
+                            <div class="work_content">
+                                <a href="#" class="arrow"><i class="lni lni-chevron-right"></i></a>
+                                <h4 class="work_title text-xl md:text-2xl"><a href="#">Yoga</a></h4>
+                                <p class="mt-2">Spa</p>
+                            </div>
+                        </div>
+                    </div> <!-- single work -->
+                </div>
+                <div class="w-full lg:w-3/12">
+                    <div class="single_item mx-auto">
+                        <div class="single_work mx-3">
+                            <div class="work_image">
+                                <img src="assets/images/paddle-board.jpg" alt="work" class="w-full">
+                            </div>
+                            <div class="work_content">
+                                <a href="#" class="arrow"><i class="lni lni-chevron-right"></i></a>
+                                <h4 class="work_title text-xl md:text-2xl"><a href="#">Paddle Board</a></h4>
+                                <p class="mt-2">Beach Playa</p>
+                            </div>
+                        </div>
+                    </div> <!-- single work -->
+                </div>
+                <div class="w-full lg:w-3/12">
+                    <div class="single_item mx-auto">
+                        <div class="single_work mx-3">
+                            <div class="work_image">
+                                <img src="assets/images/pool-voleyball.jpg" alt="work" class="w-full">
+                            </div>
+                            <div class="work_content">
+                                <a href="#" class="arrow"><i class="lni lni-chevron-right"></i></a>
+                                <h4 class="work_title text-xl md:text-2xl"><a href="#">Pool Voleyball</a></h4>
+                                <p class="mt-2">Beach Club</p>
+                            </div>
+                        </div>
+                    </div> <!-- single work -->
+                </div>
+                <div class="w-full lg:w-3/12">
+                    <div class="single_item mx-auto">
+                        <div class="single_work mx-3">
+                            <div class="work_image">
+                                <img src="assets/images/guitar.jpg" alt="work" class="w-full">
+                            </div>
+                            <div class="work_content">
+                                <a href="#" class="arrow"><i class="lni lni-chevron-right"></i></a>
+                                <h4 class="work_title text-xl md:text-2xl"><a href="#">Guitar Player</a></h4>
+                                <p class="mt-2">Wink</p>
+                            </div>
+                        </div>
+                    </div> <!-- single work -->
+                </div>
+                <div class="w-full lg:w-3/12">
+                    <div class="single_item mx-auto">
+                        <div class="single_work mx-3">
+                            <div class="work_image">
+                                <img src="assets/images/snorkeling.jpg" alt="work" class="w-full">
+                            </div>
+                            <div class="work_content">
+                                <a href="#" class="arrow"><i class="lni lni-chevron-right"></i></a>
+                                <h4 class="work_title text-xl md:text-2xl"><a href="#">Snorkeling</a></h4>
+                                <p class="mt-2">Beach Area</p>
+                            </div>
+                        </div>
+                    </div> <!-- single work -->
+                </div>
+                <div class="w-full lg:w-3/12">
+                    <div class="single_item mx-auto">
+                        <div class="single_work mx-3">
+                            <div class="work_image">
+                                <img src="assets/images/learn-spanish.jpg" alt="work" class="w-full">
+                            </div>
+                            <div class="work_content">
+                                <a href="#" class="arrow"><i class="lni lni-chevron-right"></i></a>
+                                <h4 class="work_title text-xl md:text-2xl"><a href="#">Spanish Lessons</a></h4>
+                                <p class="mt-2">Beach Club</p>
+                            </div>
+                        </div>
+                    </div> <!-- single work -->
+                </div>
+            </div> <!-- row -->
+        </div>
+    </div> <!-- container -->
+</section>
