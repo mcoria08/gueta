83bb2f3d1098b93bcc933edb5c1e675280ec0129
Miguel Coria <mcoria08@gmail.com>
2023-02-17 12:30:28 -0700

    Creating pipeline

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,19 +45,7 @@
         <div class="header_weather d-none d-md-block m-0 p-0  col-md-4  order-md-3  ">
             <div class="container d-flex">
                 <div>
-                    <a class="weatherwidget-io" href="https://forecast7.com/en/40d71n74d01/new-york/"
-                       data-label_1="NEW YORK" data-label_2="WEATHER" data-theme="original">NEW YORK WEATHER</a>
-                    <script>
-                        !function (d, s, id) {
-                            var js, fjs = d.getElementsByTagName(s)[0];
-                            if (!d.getElementById(id)) {
-                                js = d.createElement(s);
-                                js.id = id;
-                                js.src = 'https://weatherwidget.io/js/widget.min.js';
-                                fjs.parentNode.insertBefore(js, fjs);
-                            }
-                        }(document, 'script', 'weatherwidget-io-js');
-                    </script>
+                    <iframe src="https://gueta.pixelesweb.com/assets/index.html" title="description"></iframe>
                 </div>
             </div>
 
