9fbbabd3cf9281bef6b32eb2d77d9dcab37fa2ac
Miguel Coria <mcoria08@gmail.com>
2023-02-17 11:52:22 -0700

    Creating pipeline

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,7 +45,20 @@
         <div class="header_weather d-none d-md-block m-0 p-0  col-md-4  order-md-3  ">
             <div class="container d-flex">
                 <div>
-                    
+                    <a class="weatherwidget-io" href="https://forecast7.com/en/22d89n109d92/cabo-san-lucas/"
+                       data-label_1="LOS CABOS" data-label_2="WEATHER" data-days="3" data-theme="original">LOS CABOS
+                        WEATHER</a>
+                    <script>
+                        !function (d, s, id) {
+                            var js, fjs = d.getElementsByTagName(s)[0];
+                            if (!d.getElementById(id)) {
+                                js = d.createElement(s);
+                                js.id = id;
+                                js.src = 'https://weatherwidget.io/js/widget.min.js';
+                                fjs.parentNode.insertBefore(js, fjs);
+                            }
+                        }(document, 'script', 'weatherwidget-io-js');
+                    </script>
                 </div>
                 <div>WEATHER</div>
 
