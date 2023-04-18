496d08bc90fc19bec9814ab5cb522099da8bf2b0
Miguel Coria <mcoria08@gmail.com>
2023-04-18 15:00:32 -0700

    Final updates.

--- resources/views/dashboard-modal-upload-image.blade.php
+++ resources/views/dashboard-modal-upload-image.blade.php
@@ -29,8 +29,8 @@
                         <input
                             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             aria-describedby="image" id="image" name="image" type="file">
-                        <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="upload_help">SVG, PNG, JPG or
-                            GIF (MAX. 800x400px).</p>
+                        <!-- <p class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="upload_help">SVG, PNG, JPG or
+                            GIF (MAX. 800x400px).</p>-->
 
                     </div>
                 </div>
